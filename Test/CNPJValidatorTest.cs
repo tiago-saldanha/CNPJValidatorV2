b9ad38a364856af4643a96768b9761c696861913
tas284 <tiago.avila.saldanha@gmail.com>
2025-05-11 16:41:07 -0300

    update-version-nuget-1.1.7

--- Test/CNPJValidatorTest.cs
+++ Test/CNPJValidatorTest.cs
@@ -8,56 +8,64 @@ namespace CNPJValidatorV2.Test
         public void ShouldValidadeCNPJ()
         {
             var cnpj = "12.345.678/0001-95";
-            var isValid = CNPJValidator.IsValid(cnpj);
-            Assert.True(isValid);
+            var expected = CNPJValidator.IsValid(cnpj);
+            Assert.True(expected);
         }
 
         [Fact(DisplayName = "ShouldNotValidadeCNPJ")]
         public void ShouldNotValidadeCNPJ()
         {
             var cnpj = "12.345.678/0001-94";
-            var isValid = CNPJValidator.IsValid(cnpj);
-            Assert.False(isValid);
+            var expected = CNPJValidator.IsValid(cnpj);
+            Assert.False(expected);
+        }
+
+        [Fact(DisplayName = "ShouldNotValidadeCNPJIfCNPJIsInvalid")]
+        public void ShouldNotValidadeCNPJIfCNPJIsInvalid()
+        {
+            var cnpj = "11.111.111/1111-11";
+            var expected = CNPJValidator.IsValid(cnpj);
+            Assert.False(expected);
         }
 
         [Fact(DisplayName = "ShouldFormatCNPJ")]
         public void ShouldFormatCNPJ()
         {
             var cnpj = "12345678000195";
-            var formattedCNPJ = cnpj.FormatCNPJ();
-            Assert.Equal("12.345.678/0001-95", formattedCNPJ);
+            var expected = cnpj.FormatCNPJ();
+            Assert.Equal("12.345.678/0001-95", expected);
         }
 
         [Fact(DisplayName = "ShouldCalculateDV")]
         public void ShouldCalculateDV()
         {
             var cnpj = "12ABC34501DE";
-            var calculatedCNPJ = CNPJValidator.CalculateDV(cnpj);
-            Assert.Equal("12ABC34501DE35", calculatedCNPJ);
+            var expected = CNPJValidator.CalculateDV(cnpj);
+            Assert.Equal("12ABC34501DE35", expected);
         }
 
         [Fact(DisplayName = "ShouldCalculateDVIsInvalid")]
         public void ShouldCalculateDVIsInvalid()
         {
             var cnpj = "12ABC34501DE";
-            var calculatedCNPJ = CNPJValidator.CalculateDV(cnpj);
-            Assert.NotEqual("12ABC34501DE37", calculatedCNPJ);
+            var expected = CNPJValidator.CalculateDV(cnpj);
+            Assert.NotEqual("12ABC34501DE37", expected);
         }
 
         [Fact(DisplayName = "ShouldCalculateDVAndFormat")]
         public void ShouldCalculateDVAndFormat()
         {
             var cnpj = "12ABC34501DE";
-            var calculatedCNPJ = CNPJValidator.CalculateDV(cnpj).FormatCNPJ();
-            Assert.Equal("12.ABC.345/01DE-35", calculatedCNPJ);
+            var expected = CNPJValidator.CalculateDV(cnpj).FormatCNPJ();
+            Assert.Equal("12.ABC.345/01DE-35", expected);
         }
 
         [Fact(DisplayName = "ShouldCalculateDVAndFormatButIsInvalid")]
         public void ShouldCalculateDVAndFormatButIsInvalid()
         {
             var cnpj = "12ABC34501DE";
-            var calculatedCNPJ = CNPJValidator.CalculateDV(cnpj).FormatCNPJ();
-            Assert.NotEqual("12.ABC.345/01DE-37", calculatedCNPJ);
+            var expected = CNPJValidator.CalculateDV(cnpj).FormatCNPJ();
+            Assert.NotEqual("12.ABC.345/01DE-37", expected);
         }
 
         [Fact(DisplayName = "ShouldNotCalculateDVIfCNPJLessThan12Digtis")]
